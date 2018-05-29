79b59d56677293d0ae7111d7ba8e64e0acb65896
alesluca00 <39696522+alesluca00@users.noreply.github.com>
2018-05-29 14:50:48 +0200

    salva stato
    salva stato

--- index.js
+++ index.js
@@ -133,99 +133,7 @@ var colors = [{
 ];
 
 
-var persone = [{
-    nome: "Francesco",
-    cognome: "Ferrante",
-    fiscale: "AAB",
-    img: "img/ferrante.jpg",
-    style: {
 
-        class:"avatar",
-        toppp: '100px'
-    }
-    }, {
-    nome: "Franco",
-    cognome: "Cason",
-    fiscale: "AAC",
-    img: "img/cason.png",
-
-    style: {
-        class:"avatar",
-        toppp: '200px'
-    }
-    }, {
-    nome: "Luigi",
-    cognome: "Rossi",
-    fiscale: "QAB",
-    img: "img/rossi.jpg",
-
-    style: {
-    class:"avatar",
-    toppp: "300px"
-    }
-    }, {
-    nome: "Anna",
-    cognome: "Bianchi",
-    fiscale: "ABB",
-    img: "img/bianchi.jpg",
-
-    style: {
-        class: "avatar",
-        toppp: '400px'
-    }
-    },
-    {
-    nome: "Carla",
-    cognome: "Neri",
-    fiscale: "ZBB",
-    img: "img/neri.jpeg",
-
-    style: {
-        class: "avatar",
-        toppp: '400px'
-    }
-    },
-    {
-    nome: "Mario",
-    cognome: "Verdi",
-    fiscale: "QWB",
-    img: "img/verdi.jpeg",
-
-    style: {
-        class: "avatar",
-        toppp: '400px'
-    }
-    },
-    {
-    nome: "Sara",
-    cognome: "Marroni",
-    fiscale: "MBB",
-    img: "img/marroni.jpg",
-
-    style: {
-        class: "avatar",
-        toppp: '400px'
-    }
-    },
-    {
-    nome: "Guido",
-    cognome: "Piano",
-    fiscale: "PBB",
-    img: "img/piano.jpg",
-    style: {
-        class: "avatar",
-        toppp: '400px'
-    }
-    },{
-        nome: "Felice",
-        cognome: "Severo",
-        fiscale: "SBB",
-        img: "img/severo.jpg",
-        style: {
-            class: "avatar",
-            toppp: '400px'
-        } 
-    }];
     var theme = [{
        nome: 'default',
        style: {
@@ -250,14 +158,50 @@ var persone = [{
      }];
 
 
-var personaSelected = null;
 var coloreSelected = null;
 var themeSelected = null;
 
 var colorsDOM = document.getElementById('colors');
-var personeDOM = document.getElementById('persone');
 var themeDOM = document.getElementById('temi');
 
+
+
+
+/*****************************/
+var persone = new Persone();
+persone.toView();
+
+for(var i=0; i < persone.personeDOM.children.length; i++) {
+    persone.personeDOM.children[i].addEventListener('click', (function( value) {
+        return function() { 
+    
+                SelectPersona(value); 
+            
+            if (coloreSelected) { 
+                    
+                ChangeColorPersona(value);
+            }
+    
+            if(themeSelected) {
+                ChangeTheme();
+            }
+        }
+    })(persone.personeDOM.children[i].fiscale));
+}
+
+
+
+for(var i = 0; i < persone.personeDOM.children.length; i++) {
+
+    persone.personeDOM.children[i].addEventListener('mousedown', (function(cognome){
+        return function(){
+            resetBorder(cognome);
+            changeBorder(cognome);
+        }
+    })(persone.personeDOM.children[i].cognome));
+}
+
+/*****************************/
 
 
 /******************************* LOGICA CHE DISEGNA *******************************************/
@@ -310,64 +254,7 @@ for (var i = 0; i < colors.length; i++) {
     }
 
 
-    var personeDOMChildren = personeDOM.children;
-    for (var i = 0; i < persone.length; i++) {
-
-    var divPersona_i = document.createElement("div");
-
-    var nome_cognome = document.createTextNode(persone[i].nome + ' ' + persone[i].cognome);
-
-    var avatar = document.createElement("img");
-    avatar.src = persone[i].img;
-    console.log(divPersona_i);
-
-    divPersona_i.appendChild(avatar);
-    divPersona_i.appendChild(nome_cognome);
-
-
-
-
-    divPersona_i.setAttribute("fiscale", persone[i].fiscale);
-
-
-    divPersona_i.classList.add(persone[i].style.class);
-    divPersona_i.fiscale = persone[i].fiscale;
-
-
-
-
-    ///////////////////////////////////////////////////////////////////////////////////ascoltatore persona selezionata
-    divPersona_i.addEventListener('click', (function( value) {
-        return function() { 
- 
-             SelectPersona(value); 
-            
-            if (coloreSelected) { 
-                 
-                ChangeColorPersona(value);
-            }
-
-            if(themeSelected) {
-                ChangeTheme()
-            }
-        }
-    })(persone[i].fiscale));
-
     
-
-    divPersona_i.addEventListener('mousedown', (function(cognome){
-        return function(){
-            resetBorder(cognome);
-            changeBorder(cognome);
-        }
-    })(persone[i].cognome));
-
-
-    personeDOM.appendChild(divPersona_i);
-
-
-}
-
 
 
 /*********************************************************************** FUNCTIONS ***********************************************************************/
@@ -384,22 +271,22 @@ console.log("tema selezionato = ", themeSelected);
 }
 
 var SelectPersona = function(id) {
-personaSelected = id;
-console.log("personaSelected = ", personaSelected);
+persone.personaSelected = id;
+console.log("personaSelected = ", persone.personaSelected);
 }
 
 var ChangeColorPersona = function() {
 console.log("changeColorPersona");
 console.log("coloreSelected = ", coloreSelected);
-console.log("personaSelected = ", personaSelected);
-console.log("personeDOM = ", personeDOM.children);
+console.log("personaSelected = ", persone.personaSelected);
+console.log("personeDOM = ", persone.personeDOM.children);
 
 //var personeDOMChildren = personeDOM.children;
 var fiscaleSelected;
 var colore;
 var nuovoColore;
 console.log("colore", colore);
-var personeDOMChildren = personeDOM.children;
+var personeDOMChildren = persone.personeDOM.children;
 for (var i = 0; i < personeDOMChildren.length; i++) {
     console.log("personeDOM[" + i + "] = ", personeDOMChildren[i]);
 
@@ -430,15 +317,15 @@ var changeBorder = function(cognome) {
 var cognome2 = cognome;
 console.log("cognome: ", cognome2);
 
-for (var i = 0; i < personeDOMChildren.length; i++) {
+for (var i = 0; i < persone.personeDOM.children.length; i++) {
 
-    for (i = 0; i < persone.length; i++) {
-        if (persone[i].cognome == cognome2) {
+    for (i = 0; i < persone.persone.length; i++) {
+        if (persone.persone[i].cognome == cognome2) {
 
            // console.log("reset", reset);
             console.log("funziona", cognome2);
 
-            personeDOMChildren[i].style.border = '3px solid blue';
+            persone.personeDOM.children[i].style.border = '3px solid blue';
             
             
         }
@@ -452,15 +339,15 @@ var resetBorder = function(cognome) {
 var cognome2 = cognome;
 console.log("cognome: ", cognome2);
 
-for (var i = 0; i < personeDOMChildren.length; i++) {
+    for (var i = 0; i < persone.personeDOM.children.length; i++) {
 
-    for (i = 0; i < persone.length; i++) {
-        
-        console.log("funziona", cognome2);
+        for (i = 0; i < persone.persone.length; i++) {
+            
+            console.log("funziona", cognome2);
 
-        personeDOM.children[i].style.border = '1px solid black'; 
+            persone.personeDOM.children[i].style.border = '1px solid black'; 
+        }
     }
-}
 }
 
 /*********************************************************************** Funzione CAMBIA TEMA ***********************************************************************/
@@ -475,26 +362,26 @@ var ChangeTheme = function(){
     var fiscaleSelected = null;
     var tema = null;
     tema = themeSelected;
-    fiscaleSelected = personeDOM.children[0].fiscale;
+    fiscaleSelected = persone.personeDOM.children[0].fiscale;
 
     console.log("tema selezionato", tema);
     console.log("Persona selezionata: ", personaSelected);
 
     
 
-    var personeDOMChildren = personeDOM.children;
+    var personeDOMChildren = persone.personeDOM.children;
     for (var i = 0; i < personeDOMChildren.length; i++) {
         console.log("personeDOM[" + i + "] = ", personeDOMChildren[i]);
 
         fiscaleSelected = personeDOMChildren[i].fiscale;
         
 
-        if (fiscaleSelected == personaSelected) {
+        if (fiscaleSelected == persone.personaSelected) {
              
             tema = themeSelected;
 
             //ciclo per rimuovere quelle gia' messe 
-            personeDOM.children[i].classList.add(tema);
+            persone.personeDOM.children[i].classList.add(tema);
         }
     }
 
@@ -504,54 +391,8 @@ var ChangeTheme = function(){
 }
 
 
-/*********************************************************************** array immagini ***********************************************************************/
-
-var arrayOfImage = [{
-
-img : 'img/ferrante.jpg',
-value :'ferrante'
-},
-{
-    img:'img/rossi.jpg',
-  value: 'rossi'
-},
-{
-   img:'img/bianchi.jpg',
-    value:'bianchi'
-},
-{
-    img:'img/cason.png',
-   value:'cason'
-}];
 
 
-/*********************************************************************** parte logica arrayOfImage ***********************************************************************/
-
-
-for (var i = 0; i < arrayOfImage.length; i++) {
-   
-    var divimage_i = document.createElement("div");
-
-    var style_image = document.createElement("img");
-
-    /*style_image.appendChild(style_image);
-    
-    style_image.src = arrayOfImage[i].img;
-
-     style_image.classList.add("style_image");
-   
-    //console.log(divimage_i);
-    style_image.setAttribute("value", arrayOfImage[i].value);
-
-
-        
-    
-    divimage_i.img = arrayOfImage[i].img;
-    divimage_i.value = arrayOfImage[i].value;*/
-    
-
-//------------------------------>controllare perchè non passa il tema nella funzione  <-----------------------------------------------------
-}
 
 
 
--- persone.js
+++ persone.js
@@ -2,102 +2,130 @@
 
 
 class Persone {
-    listPersone = [{
-        nome: "Francesco",
-        cognome: "Ferrante",
-        fiscale: "AAB",
-        img: "img/ferrante.jpg",
-        style: {
-    
+
+    constructor() {
+
+        this.persone = [{
+            nome: "Francesco",
+            cognome: "Ferrante",
+            fiscale: "AAB",
+            img: "img/ferrante.jpg",
+            style: {
+        
+                class:"avatar",
+                toppp: '100px'
+            }
+            }, {
+            nome: "Franco",
+            cognome: "Cason",
+            fiscale: "AAC",
+            img: "img/cason.png",
+        
+            style: {
+                class:"avatar",
+                toppp: '200px'
+            }
+            }, {
+            nome: "Luigi",
+            cognome: "Rossi",
+            fiscale: "QAB",
+            img: "img/rossi.jpg",
+        
+            style: {
             class:"avatar",
-            toppp: '100px'
-        }
-        }, {
-        nome: "Franco",
-        cognome: "Cason",
-        fiscale: "AAC",
-        img: "img/cason.png",
-    
-        style: {
-            class:"avatar",
-            toppp: '200px'
-        }
-        }, {
-        nome: "Luigi",
-        cognome: "Rossi",
-        fiscale: "QAB",
-        img: "img/rossi.jpg",
-    
-        style: {
-        class:"avatar",
-        toppp: "300px"
-        }
-        }, {
-        nome: "Anna",
-        cognome: "Bianchi",
-        fiscale: "ABB",
-        img: "img/bianchi.jpg",
-    
-        style: {
-            class: "avatar",
-            toppp: '400px'
-        }
-        },
-        {
-        nome: "Carla",
-        cognome: "Neri",
-        fiscale: "ZBB",
-        img: "img/neri.jpeg",
-    
-        style: {
-            class: "avatar",
-            toppp: '400px'
-        }
-        },
-        {
-        nome: "Mario",
-        cognome: "Verdi",
-        fiscale: "QWB",
-        img: "img/verdi.jpeg",
-    
-        style: {
-            class: "avatar",
-            toppp: '400px'
-        }
-        },
-        {
-        nome: "Sara",
-        cognome: "Marroni",
-        fiscale: "MBB",
-        img: "img/marroni.jpg",
-    
-        style: {
-            class: "avatar",
-            toppp: '400px'
-        }
-        },
-        {
-        nome: "Guido",
-        cognome: "Piano",
-        fiscale: "PBB",
-        img: "img/piano.jpg",
-        style: {
-            class: "avatar",
-            toppp: '400px'
-        }
-        },{
-            nome: "Felice",
-            cognome: "Severo",
-            fiscale: "SBB",
-            img: "img/severo.jpg",
+            toppp: "300px"
+            }
+            }, {
+            nome: "Anna",
+            cognome: "Bianchi",
+            fiscale: "ABB",
+            img: "img/bianchi.jpg",
+        
             style: {
                 class: "avatar",
                 toppp: '400px'
-            } 
-    }];
+            }
+            },
+            {
+            nome: "Carla",
+            cognome: "Neri",
+            fiscale: "ZBB",
+            img: "img/neri.jpeg",
+        
+            style: {
+                class: "avatar",
+                toppp: '400px'
+            }
+            },
+            {
+            nome: "Mario",
+            cognome: "Verdi",
+            fiscale: "QWB",
+            img: "img/verdi.jpeg",
+        
+            style: {
+                class: "avatar",
+                toppp: '400px'
+            }
+            },
+            {
+            nome: "Sara",
+            cognome: "Marroni",
+            fiscale: "MBB",
+            img: "img/marroni.jpg",
+        
+            style: {
+                class: "avatar",
+                toppp: '400px'
+            }
+            },
+            {
+            nome: "Guido",
+            cognome: "Piano",
+            fiscale: "PBB",
+            img: "img/piano.jpg",
+            style: {
+                class: "avatar",
+                toppp: '400px'
+            }
+            },{
+                nome: "Felice",
+                cognome: "Severo",
+                fiscale: "SBB",
+                img: "img/severo.jpg",
+                style: {
+                    class: "avatar",
+                    toppp: '400px'
+                } 
+        }];
+
+        this.personaSelected = null;
+
+        this.personeDOM = document.getElementById('persone');
+    }
+
+
 
     toView() {
+
+        for (var i = 0; i < this.persone.length; i++) {
+
+            var divPersona_i = document.createElement("div");
+            var nome_cognome = document.createTextNode(this.persone[i].nome + ' ' + this.persone[i].cognome);
+            var avatar = document.createElement("img");
+
+            avatar.src = this.persone[i].img;
+
+            divPersona_i.appendChild(avatar);
+            divPersona_i.appendChild(nome_cognome);
+            divPersona_i.setAttribute("fiscale", this.persone[i].fiscale);
+            divPersona_i.classList.add(this.persone[i].style.class);
+            divPersona_i.fiscale = this.persone[i].fiscale;
+            this.personeDOM.appendChild(divPersona_i);
         
+        }
+
     }
+
        
 }
